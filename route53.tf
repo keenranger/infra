fe0942151de96436e4b6f5156580435812f44324
Hankyeol Kyung <kghnkl0103@gmail.com>
2024-03-12 06:12:06 +0000

    remove unused instance

--- route53.tf
+++ route53.tf
@@ -2,11 +2,3 @@ resource "aws_route53_zone" "keenranger" {
   name    = "keenranger.dev"
   comment = "Managed by OpenTofu"
 }
-
-resource "aws_route53_record" "dev" {
-  zone_id = aws_route53_zone.keenranger.zone_id
-  name    = "dev.keenranger.dev"
-  type    = "A"
-  ttl     = "300"
-  records = [aws_eip.dev.public_ip]
-}
